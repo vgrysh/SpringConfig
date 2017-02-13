349c5b7608d8ba4dd3307b501e74f82046595a5e
Volodymyr Gryshaienko <vgrysh@softserveinc.com>
2017-02-13 14:40:13 +0200

    init

---
+++ src/main/java/SpringCloud/TestApplication.java
@@ -0,0 +1,29 @@
+package SpringCloud;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableAutoConfiguration
+@ComponentScan
+@RestController
+@SpringBootApplication
+public class TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+
+    @Value("${author:World!}")
+    String author;
+
+    @RequestMapping("/")
+    String hello() {
+        return "Hello " + author + "!";
+    }
+}
